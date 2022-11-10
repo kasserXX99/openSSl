ce454634b8d4c001e54ef1df03c33e16943941ec
ameralshabebi <69112877+ameralshabebi@users.noreply.github.com>
2022-11-10 16:19:51 +0300

    check the validate function it pritn undifined while it's waiting for the result

--- src/app.js
+++ src/app.js
@@ -1,12 +1,22 @@
 import express from "express";
 import { validate, create } from "./utils/certificate.js";
-
+import multer from "multer";
+import fs from "fs";
 // const __filename = path.dirname(fileURLToPath(import.meta.url));
 // const location = path.join(__filename, "./longLoop.js");
 
 const app = express();
 app.use(express.json());
-
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, `./apple_certificate`);
+	},
+	filename: function (req, file, cb) {
+		console.log(req.files);
+		cb(null, file.originalname);
+	},
+});
+const upload = multer({ storage: storage });
 app.get("/certificate", async (req, res, next) => {
 	const { merchant_name } = req.body;
 	try {
@@ -20,12 +30,29 @@ app.get("/certificate", async (req, res, next) => {
 		next(err);
 	}
 });
-app.get("/testing", async (req, res) => {
-	res.json("hi there");
+
+app.post("/validate_certificate", upload.single("cer"), async (req, res) => {
+	const { merchant_name } = req.body;
+	try {
+		const result = await validate(merchant_name);
+		if (result == "valid") {
+			res
+				.status(201)
+				.json({ result: "certificate was validated successfully" });
+		}
+	} catch (e) {
+		throw e;
+	}
 });
 
-app.post("/add_certificate", (req, res) => {
-	res.send(req.body.certificate);
+app.post("/testing", upload.single("cer"), async (req, res) => {
+	// console.log(req.body);
+	console.log(req.file);
+	fs.readFile(req.file.path, (err, data) => {
+		if (err) throw err;
+		const buff = Buffer.from(data, "base64");
+		res.json(buff.toString("utf-8"));
+	});
 });
 
 app.use((err, req, res, next) => {
--- src/utils/certificate.js
+++ src/utils/certificate.js
@@ -3,13 +3,76 @@ import util from "node:util";
 import { exec } from "child_process";
 import fs from "fs";
 import { fileURLToPath } from "url";
-import path, { resolve } from "path";
+import path from "path";
 
 const __filename = path.dirname(fileURLToPath(import.meta.url));
 
-const execute = async (format, merchantName, res, rej, command) => {
+const validate = async (merchant_name) => {
+	return new Promise(async (res, rej) => {
+		const key = path.join(
+			__filename,
+			`../../apple_certificate/${merchant_name}.key`
+		);
+		const cer = path.join(
+			__filename,
+			`../../apple_certificate/${merchant_name}.cer`
+		);
+
+		const res1 = await execute(
+			"cer",
+			merchant_name,
+			res,
+			rej,
+			`openssl pkey -pubout -in ${key} | openssl md5`
+		);
+		const res2 = await execute(
+			"cer",
+			merchant_name,
+			res,
+			rej,
+			`openssl x509 -pubkey -inform der -in ${cer} -noout | openssl md5`
+		);
+		if (res1 === res2) {
+			console.log("res1", res1, "res2", res2);
+			console.log(res1 === res2);
+			res("valid");
+		}
+	});
+};
+const create = (name = "name") => {
+	if (
+		fs.existsSync(path.join(__filename, `../../apple_certificate/${name}.csr`))
+	) {
+		return "exist";
+	}
+	const key = path.join(__filename, `../../apple_certificate/${name}.key`);
+	const csr = path.join(__filename, `../../apple_certificate/${name}.csr`);
+	return new Promise(async (res, rej) => {
+		await execute(
+			"key",
+			name,
+			res,
+			rej,
+			`openssl ecparam -out ${key} -name prime256v1 -genkey`
+		);
+		await execute(
+			"csr",
+			name,
+			res,
+			rej,
+			`openssl req -new -sha256 -key ${key} -nodes -out ${csr} -subj "/C=SA/ST=Central/L=Ghernatah/O=Dinero, Inc./OU=IT/CN=Diner.com"`
+		);
+	});
+};
+const execute = async (format, merchantName = null, res, rej, command) => {
 	try {
 		const ex = util.promisify(exec);
+		if (format === "cer") {
+			const { stdout, stderr } = await ex(command);
+			res(stdout);
+		}
+		{
+		}
 		if (format === "csr") {
 			const { stdout, stderr } = await ex(command);
 			fs.readFile(
@@ -32,32 +95,5 @@ const execute = async (format, merchantName, res, rej, command) => {
 		console.log("error while excuting the command");
 		rej(err);
 	}
-};
-
-const validate = (cer) => {};
-const create = (name = "name") => {
-	if (
-		fs.existsSync(path.join(__filename, `../../apple_certificate/${name}.csr`))
-	) {
-		return "exist";
-	}
-	const key = path.join(__filename, `../../apple_certificate/${name}.key`);
-	const csr = path.join(__filename, `../../apple_certificate/${name}.csr`);
-	return new Promise(async (res, rej) => {
-		await execute(
-			"key",
-			name,
-			res,
-			rej,
-			`openssl ecparam -out ${key} -name prime256v1 -genkey`
-		);
-		await execute(
-			"csr",
-			name,
-			res,
-			rej,
-			`openssl req -new -sha256 -key ${key} -nodes -out ${csr} -subj "/C=US/ST=Utah/L=Lehi/O=Your Company, Inc./OU=IT/CN=yourdomain.com"`
-		);
-	});
 };
 export { validate, create };
